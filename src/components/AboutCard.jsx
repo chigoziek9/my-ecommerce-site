911a355c34b6c7219a8083802ff7fefb5c85c365
Nony <nonybrighto@gmail.com>
2025-06-20 18:46:10 +0100

    about mobile responsive

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,14 +1,16 @@
 export default function AboutCard({ image, title, text }) {
   return (
-    <div className=" w-70 h-63 mt-15 bg-amber-50 border shadow-md hover:shadow-lg transition-shadow duration-300">
+    <div className="w-[90%] max-w-sm md:w-70 h-auto mt-6 bg-amber-50 border shadow-md hover:shadow-lg transition-shadow duration-300 mx-auto">
       <img
-        className=" w-30 h-30  p-5 mx-20"
+        className="w-24 h-24 md:w-30 md:h-30 p-4 mx-auto"
         src={image}
         alt={title}
       />
       <div className="p-4">
-        <h2 className="text-4xl text-center font-extrabold mb-2">{title}</h2>
-        <p className="text-gray-600 text-sm text-center">{text}</p>
+        <h2 className="text-2xl md:text-4xl text-center font-extrabold mb-2">
+          {title}
+        </h2>
+        <p className="text-gray-600 text-sm md:text-base text-center">{text}</p>
       </div>
     </div>
   );
--- src/components/AboutImage.jsx
+++ src/components/AboutImage.jsx
@@ -1,14 +1,18 @@
 export default function AboutImage({ image, title, text }) {
   return (
-    <div className=" w-93 h-101 mt-15 bg-amber-50 border shadow-md hover:shadow-lg transition-shadow duration-300">
+    <div className="w-[95%] max-w-3xl h-auto mt-6 bg-amber-50 border shadow-md hover:shadow-lg transition-shadow duration-300 mx-auto">
       <img
-        className=" w-93 h-100"
+        className="w-full h-auto object-cover"
         src={image}
         alt={title}
       />
       <div className="p-4">
-        <h2 className="text-4xl text-center font-extrabold mb-2">{title}</h2>
-        <p className="text-gray-600 text-xl text-center">{text}</p>
+        <h2 className="text-2xl md:text-4xl text-center font-extrabold mb-2">
+          {title}
+        </h2>
+        <p className="text-gray-600 text-base md:text-xl text-center">
+          {text}
+        </p>
       </div>
     </div>
   );
